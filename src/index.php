4525c7bad83d9b875dc5879c6a2850afbfe3ac0a
Danixe32 <daniel.prostak@op.pl>
2025-05-06 12:07:11 +0200

    add navbar

--- src/index.php
+++ src/index.php
@@ -6,8 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Big Win Casino</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    <h1>Hello World!</h1>
+    <?php require_once('components/nav.php')?>
 </body>
 </html>
