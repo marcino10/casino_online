b98f7aeb18e5236b9a41f564df5e34ba6cbd5632
Paweł Sarzała <129670633+DarkCrocsDotCom@users.noreply.github.com>
2025-05-06 12:03:00 +0200

    add index and poker page

--- src/index.php
+++ src/index.php
@@ -6,8 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Big Win Casino</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    <h1>Hello World!</h1>
+
+<main>
+    <section class="hero">
+        <h1 class="title">Welcome to Big Win Casino</h1>
+        <div class="inventory">
+            <p class="subtitle">Experience the thrill of gaming with our exclusive poker game.</p>
+            <a href="poker.php" class="button">Start Playing</a>
+            <!--        <button></button>-->
+        </div>
+    </section>
+</main>
+
 </body>
 </html>
