b093fab6a1afea45920fe8514f6d881973ed385c
Danixe32 <daniel.prostak@op.pl>
2025-05-21 14:24:59 +0200

    add pop-up not done yet

--- src/public/js/pokerReveal.js
+++ src/public/js/pokerReveal.js
@@ -43,4 +43,18 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+// popup
+document.addEventListener("DOMContentLoaded", () => {
+  const popup = document.querySelector(".pop-up");
+  const helpIcon = document.querySelector("#helpIcon");
+  const closeBtn = document.querySelector(".close_pop-up");
+
+  helpIcon?.addEventListener("click", () => {
+    popup?.classList.toggle("visible");
+  });
+
+  closeBtn?.addEventListener("click", () => {
+    popup?.classList.remove("visible");
+  });
+});
 
